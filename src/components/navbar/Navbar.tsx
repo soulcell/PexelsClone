d91b771e7db76ce59f7ad6c796dce9780ced5ecd
soulcell <arsen332211@gmail.com>
2022-12-16 17:20:48 +0300

    fixed useEffect dependencies array in Navbar component, removed unused code lines from index.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar({
       window.addEventListener("scroll", handleScroll);
       return () => window.removeEventListener("scroll", handleScroll);
     } else setFixed(false);
-  }, [handleScroll]);
+  }, [handleScroll, isHomePage]);
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-
-const router = createBrowserRouter([])
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
