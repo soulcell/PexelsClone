837717924cf162c51d740c07015863ce1711f4f8
soulcell <arsen332211@gmail.com>
2022-12-22 15:49:37 +0300

    added link to author and to a photo to PhotoCard

--- src/components/photo-card/PhotoCard.tsx
+++ src/components/photo-card/PhotoCard.tsx
@@ -5,10 +5,17 @@ export interface PhotoCardProps {
   photo: Photo;
 }
 
-export default function PhotoCard(props: PhotoCardProps): JSX.Element {
+export default function PhotoCard({ photo }: PhotoCardProps): JSX.Element {
   return (
     <>
-      <img className={styles.image} src={props.photo.src.large2x}></img>
+      <article className={`${styles.card} ${styles.overlay}`}>
+        <a href={photo.url}>
+          <img className={styles.image} src={photo.src.large2x} />
+        </a>
+        <a className={styles.photographer} href={photo.photographer_url}>
+          <span className={styles.name}>{photo.photographer}</span>
+        </a>
+      </article>
     </>
   );
 }
