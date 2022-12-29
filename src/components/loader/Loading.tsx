b47c2c13aa5f3e0b994144845e0f0792eb1bad30
soulcell <arsen332211@gmail.com>
2022-12-29 22:21:53 +0300

    moved animation inside css

--- src/components/loader/Loading.tsx
+++ src/components/loader/Loading.tsx
@@ -1,10 +1,24 @@
-import SVG from "../svg/SVG";
 import styles from "./Loading.module.css";
 
 export default function Loading(): JSX.Element {
   return (
-    <div className={styles.root}>
-      <SVG icon="Loading" />
+    <div className={styles["root"]}>
+      <div className={styles["loadingio-spinner-spinner-tlce58hty5"]}>
+        <div className={styles["ldio-ouxrbfu7gz"]}>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
     </div>
   );
 }
